dd45d4b451132d231d65d5cf20945a8446ca2d1c
Brent Fitzgerald <burnto@gmail.com>
2021-08-22 22:17:54 -0700

    add animated variant of previous still

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 import p5 from "p5";
 
 const sketchImports = [
+  import("./sketches/2021-08-21-1"),
   import("./sketches/2021-08-21"),
   import("./sketches/2021-08-20-3"),
   import("./sketches/2021-08-20-2"),
