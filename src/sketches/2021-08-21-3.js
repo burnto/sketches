63444b43af71e6281d42c65593e9266b0d822cc6
Brent Fitzgerald <burnto@gmail.com>
2021-08-23 20:32:36 -0700

    add eye of sauron

--- src/sketches/2021-08-21-3.js
+++ src/sketches/2021-08-21-3.js
@@ -1,5 +1,4 @@
-let sketch = (p) => {
-
+export default (p) => {
   const totalWidth = 600;
   const totalHeight = 600;
   const padding = 40;
@@ -10,25 +9,38 @@ let sketch = (p) => {
   const iters = 50;
   const framesPerCycle = 1000;
 
-  let endColor = p.color(0, 0, 180, 200);
-  let startColor = p.color(0, 180, 180, 0);
+  let endColor = p.color(0, 0, 180, 100);
+  let startColor = p.color(0, 180, 180, 100);
 
   p.setup = () => {
     p.createCanvas(totalWidth, totalHeight);
     p.background(255);
     p.noFill();
-    p.stroke(0);
-    p.strokeWeight(2);
+    p.stroke(0, 0, 0, 100);
+    p.strokeWeight(1);
+    p.noFill;
+  };
 
+  p.draw = () => {
+    p.background(240, 240, 240);
     for (let i = 0; i < iters; i++) {
+      const th = i * Math.PI * 2 / iters;
+      const x = w / 2 + (Math.sin(th) * w / 2);
+      const y = h / 2 + (Math.cos(th) * h / 2);
+
+      p.push();
+      p.translate(padding + x, padding + y);
       for (let j = 0; j < iters; j++) {
-        const th = i * Math.PI * 2 / iters;
-        const x = w / 2 + (Math.sin(th) * w / 4);
-        const y = h / 2 + (Math.cos(th) * h / 4);
-        p.point(padding + x, padding + y);
+
+        const th2 = (p.frameCount / 1000) + 2 * Math.PI * j / iters;
+        const color = p.lerpColor(startColor, endColor, j / iters)
+        p.stroke(color);
+        p.push();
+        p.rotate(th2);
+        p.line(0, 0, 300, 0);
+        p.pop();
       }
+      p.pop();
     }
   }
 }
-
-export default sketch;
