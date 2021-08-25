f402a6afa2ceeee01d0f10a86437f4ec1ad78e1e
Brent Fitzgerald <burnto@gmail.com>
2021-08-25 13:44:30 -0700

    use esbuild on js

--- .eleventy.js
+++ .eleventy.js
@@ -1,21 +1,18 @@
 let Nunjucks = require("nunjucks");
 
 module.exports = (config) => {
-  config.addPassthroughCopy("src/entries");
-  config.addPassthroughCopy("src/load.js");
 
+  config.addWatchTarget("./src/**/*");
+  config.addWatchTarget("./src/_includes");
   let nunjucksEnvironment = new Nunjucks.Environment(
     new Nunjucks.FileSystemLoader("src/_includes")
   );
 
   config.setLibrary("njk", nunjucksEnvironment);
 
-  config.addWatchTarget("./src/_includes");
-
   return {
     dir: {
       input: "src",
-      // output: "public"
     }
   }
 };
