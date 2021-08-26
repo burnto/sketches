56c06df7c1a0ddae299a0fd1e7e7a776a2df5b10
Brent Fitzgerald <burnto@gmail.com>
2021-08-26 16:15:15 -0700

    add sitemap and robots.txt

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,20 @@
 let Nunjucks = require("nunjucks");
+const format = require('date-fns/format')
 
 module.exports = (config) => {
 
   config.addWatchTarget("./src/**/*");
   config.addWatchTarget("./src/_includes");
-  let nunjucksEnvironment = new Nunjucks.Environment(
-    new Nunjucks.FileSystemLoader("src/_includes")
-  );
+  // let nunjucksEnvironment = new Nunjucks.Environment(
+  //   new Nunjucks.FileSystemLoader("src/_includes")
+  // );
 
-  config.setLibrary("njk", nunjucksEnvironment);
+  // add `date` filter
+  config.addFilter('date', function (date, dateFormat) {
+    return format(date, dateFormat)
+  })
+
+  // config.setLibrary("njk", nunjucksEnvironment);
 
   return {
     dir: {
---
+++ src/_data/site.js
@@ -0,0 +1,6 @@
+module.exports = function () {
+  return {
+    url: 'https://bloc.art'
+  }
+}
+
