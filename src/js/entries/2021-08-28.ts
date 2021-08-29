61396ba7f688ba6d2dafd3a946036596b82f09f2
Brent Fitzgerald <burnto@gmail.com>
2021-08-28 20:02:42 -0700

    update colors (and leave settable)

--- src/js/entries/2021-08-28.ts
+++ src/js/entries/2021-08-28.ts
@@ -1,6 +1,10 @@
 import { MyP5 } from "../types";
 
 export default (p: MyP5) => {
+  let skyPicker: any;
+  let waterPicker1: any;
+  let waterPicker2: any;
+
   p.setup = () => {
     p.createCanvas(p.currentWidth(), p.currentWidth());
     p.noStroke();
@@ -8,15 +12,20 @@ export default (p: MyP5) => {
     p.stroke(255);
     p.fill(0);
     p.frameRate(30);
+    skyPicker = p.createColorPicker("#F037A5");
+    waterPicker1 = p.createColorPicker("#1E3163");
+    waterPicker2 = p.createColorPicker("#2D46B9");
   };
   let noiseScale = 0.01;
 
   p.draw = () => {
-    p.background(40, 40, 40);
+    p.background(skyPicker.color());
     for (var i = 0; i < 7; i++) {
+      let c = p.lerpColor(waterPicker1.color(), waterPicker2.color(), i / 6);
+      p.fill(c);
       p.beginShape();
       for (let x = 0; x <= p.width + 100; x += 30) {
-        const noiseVal = p.noise(x * noiseScale, i + 100);
+        const noiseVal = p.noise(x * noiseScale, i + 1);
         const sinVal = Math.sin(p.radians(p.frameCount * (i + 1) + x));
         let y =
           p.width / 10 +
