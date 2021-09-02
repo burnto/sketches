a2f7cfcc93ae028fc86bde40b5f1e5f2a3176245
Brent Fitzgerald <burnto@gmail.com>
2021-09-02 09:16:24 -0700

    remove collections; will use a different projects

--- .eleventy.js
+++ .eleventy.js
@@ -5,10 +5,16 @@ module.exports = (config) => {
 
   config.addWatchTarget("./src/**/*");
   config.addWatchTarget("./src/_includes");
+  // let nunjucksEnvironment = new Nunjucks.Environment(
+  //   new Nunjucks.FileSystemLoader("src/_includes")
+  // );
+
   // add `date` filter
   config.addFilter('date', function (date, dateFormat) {
     return format(date, dateFormat)
   })
+
+  // config.setLibrary("njk", nunjucksEnvironment);
 
   return {
     dir: {
