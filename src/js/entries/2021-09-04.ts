4497b7e91b55fa42e93035e24bd3545ff6053b94
Brent Fitzgerald <burnto@gmail.com>
2021-09-04 20:45:03 -0700

    add spherical line sketch

---
+++ src/js/entries/2021-09-04.ts
@@ -0,0 +1,74 @@
+import { MyP5 } from "../types";
+import p5 from "p5";
+import { hashContains } from "../helpers";
+
+let sketch = (p: MyP5) => {
+  const fibSphere = (samples: number = 1) => {
+    let points = new Array<p5.Vector>();
+    const phi = Math.PI * (3 - Math.sqrt(5));
+
+    for (let i = 0; i < samples; i++) {
+      const y = 1 - (i / (samples - 1)) * 2; // y goes from 1 to -1
+      const radius = Math.sqrt(1 - y * y); //  # radius at y
+      const theta = phi * i; //  # golden angle increment
+      const x = Math.cos(theta) * radius;
+      const z = Math.sin(theta) * radius;
+      const v = p.createVector(x, y, z);
+      points.push(v);
+    }
+    return points;
+  };
+
+  p.colorMode(p.HSB, 1, 1, 1, 1);
+  const startColor = p.color(1, 0.5, 1, 1);
+  const endColor = p.color(0.5, 0.5, 1, 1);
+
+  p.setup = () => {
+    p.createCanvas(p.currentWidth(), p.currentWidth(), p.WEBGL);
+    p.frameRate(30);
+    p.strokeWeight(0.5);
+    p.noFill();
+
+    let gif;
+    if (hashContains("gif")) {
+      console.log("gif enabled");
+      gif = {
+        startLoop: 1,
+        endLoop: 2,
+        fileName: "camps",
+        options: {
+          quality: 5,
+        },
+        download: hashContains("download"),
+        open: hashContains("open"),
+      };
+    }
+    p.createLoop({
+      duration: 6,
+      gif,
+    });
+  };
+  p.draw = () => {
+    p.rotateY(p.animLoop.theta);
+    p.rotateZ(p.animLoop.theta);
+    p.background(0);
+    const numCoords = Math.sin(p.animLoop.theta) * 200;
+    let coords = fibSphere(300 + numCoords);
+    p.scale(120);
+    p.beginShape();
+    for (let i = 0; i < coords.length; i++) {
+      const c = p.lerpColor(
+        startColor,
+        endColor,
+        0.5 + Math.cos(p.radians(i * 1) + p.animLoop.theta) / 4
+      );
+      p.stroke(c);
+      let v = coords[i];
+      p.point(v.x, v.y, v.z);
+      p.vertex(v.x, v.y, v.z);
+    }
+    p.endShape();
+  };
+};
+
+export default sketch;
